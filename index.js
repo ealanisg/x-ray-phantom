c2c6bb00ca569a63d1a32274432f726bcaa3741c
Cayley Humphries <github@cayley.me>
2021-06-29 11:58:26 -0700

    Simplify and update to latest Nightmare

--- index.js
+++ index.js
@@ -1,92 +1,44 @@
-/**
- * Module Dependencies
- */
-
-var debug = require('debug')('x-ray:phantom');
-var normalize = require('normalizeurl');
-var Nightmare = require('nightmare');
-var wrapfn = require('wrap-fn');
-
-/**
-* Export `driver`
-*/
-
-module.exports = driver;
-
-/**
-* Initialize the `driver`
-* with the following `options`
-*
-* @param {Object} options
-* @param {Function} fn
-* @return {Function}
-* @api public
-*/
-
-function driver(options, fn) {
-  if ('function' == typeof options) fn = options, options = {};
-  options = options || {};
-  fn = fn || phantom;
-  var nightmare = new Nightmare(options);
+const debug = require('debug')('x-ray:phantom');
+const normalize = require('normalizeurl');
+const Nightmare = require('nightmare');
 
 
-  return function phantom_driver(ctx, done) {
-    debug('going to %s', ctx.url);
+function makeDriver(opts) {
+  opts = opts || {};
 
+	return function driver(ctx, callback) {
+		debug('going to %s', ctx.url);
+
+    const nightmare = new Nightmare(opts);
+
+    // Setup standard listeners
     nightmare
-      .on('error', error)
-      .on('timeout', function(timeout) {
-        return done(new Error(timeout));
-      })
-      .on('resourceReceived', function(resource) {
+      .on('error', (msg) => debug('client-side javascript error %s', msg))
+      .on('timeout', (timeout) => callback(new Error(timeout)))
+      .on('resourceReceived', (resource) => {
         if (normalize(resource.url) == normalize(ctx.url)) {
           debug('got response from %s: %s', resource.url, resource.status);
           ctx.status = resource.status;
         };
       })
-      .on('urlChanged', function(url) {
+      .on('urlChanged', (url) => {
         debug('redirect: %s', url);
         ctx.url = url;
+      });
+    
+    // Get all page content
+    nightmare
+      .goto(ctx.url)
+      .evaluate(() => {
+        // Get the entire document contents
+        return document.documentElement.outerHTML;
       })
-
-    wrapfn(fn, select)(ctx, nightmare);
-
-    function select(err, ret) {
-      if (err) return done(err);
-
-      nightmare
-        .evaluate(function() {
-          return document.documentElement.outerHTML;
-        }, function(body) {
-          ctx.body = body;
-        })
-        .run(function(err) {
-          if (err) return done(err);
-          debug('%s - %s', ctx.url, ctx.status);
-          done(null, ctx);
-        });
-    };
-  }
+      .then(body => {
+        ctx.body = body;
+        debug('%s - %s', ctx.url, ctx.status);
+        callback(null, ctx);
+      });
+	}
 }
 
-/**
- * Default phantom driver
- *
- * @param {HTTP Context} ctx
- * @param {Nightmare} nightmare
- * @param {Function} fn
- */
-
-function phantom(ctx, nightmare) {
-  return nightmare.goto(ctx.url);
-}
-
-/**
-* Phantom errors go here
-*
-* @param {String} msg
-*/
-
-function error(msg) {
-  debug('client-side javascript error %s', msg);
-}
+module.exports = makeDriver;
--- test.js
+++ test.js
@@ -1,28 +1,32 @@
-/**
- * Module Dependencies
- */
+const Xray = require('x-ray');
+const driver = require('./');
+const expect = require('chai').expect;
 
-var crawler = require('x-ray-crawler');
-var driver = require('./');
+const x = Xray()
+  .driver(driver());
 
-/**
- * Setup the driver
- */
+// Wrapper to help writing async tests
+const awaitCrawl = async (inst) => new Promise((res, err) => inst.then(res).catch(err));
 
-function *phantom(ctx, nightmare) {
-  nightmare.goto(ctx.url)
-  return nightmare;
-}
+describe('x-ray-phantom', () => {
+  it('correctly crawls sites', async () => {
+    const title = await awaitCrawl(x('https://www.thebodyshop.com/en-ca/sale/h/h00008', 'title'));
+    expect(title).to.equal('Beauty & Skincare Sale | Makeup Sale | The Body Shop®');
+  });
 
-/**
- * Crawl google
- */
+  it('handles complex examples', async () => {
+    const url = 'https://www.thebodyshop.com/en-ca/';
+    const scope = '.amp-dc-card-item';
+    const fields = [{
+      subHeadline: '.amp-dc-card-item__body-copy-1 h2:nth-child(1)',
+      headline: '.amp-dc-card-item__body-copy-1 h2:nth-child(2)',
+      details: '.amp-dc-card-item__body-copy-1 p:nth-child(3)',
+      legal: '.amp-dc-card-item__body-copy-1 p:nth-child(4)',
+      learnMoreUrl: 'a@href',
+      imageUrl: 'img@src',
+    }];
 
-crawler('http://google.com')
-  .driver(driver(phantom))
-  .on('response', function($, ctx) {
-    console.log('%s - %s', ctx.status, $('title').text().trim());
-  })
-  .crawl(function(err) {
-    if (err) throw err;
-  })
+    const output = await awaitCrawl(x(url, scope, fields));
+    console.log(output);
+  });
+});
